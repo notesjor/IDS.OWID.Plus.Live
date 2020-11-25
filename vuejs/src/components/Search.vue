e5e9c90bbf8952b446aedb7cd54417f1404621d7
Jan Oliver Rüdiger <github@jan-oliver-ruediger.de>
2020-11-25 13:34:15 +0100

    // commit

--- vuejs/src/components/Search.vue
+++ vuejs/src/components/Search.vue
@@ -6,26 +6,62 @@
       </v-expansion-panel-header>
       <v-expansion-panel-content>
         <v-row>
-          <v-col cols="11">
-            <v-tabs vertical style="min-height:181px">
+          <v-col>
+            <v-tabs>
               <v-tab>
-                N=1
+                <v-tooltip right>
+                  <template v-slot:activator="{ on, attrs }">
+                    <span v-bind="attrs" v-on="on">N=1</span>
+                  </template>
+                  <span>
+                    <strong>Hinweis: </strong>N=1 erlaubt es, nach einzelnen
+                    Token (Layer: Wortform, Lemma oder POS) zu suchen.<br />
+                    <strong>Beispiele: </strong>Wortform = berlin | Lemma = sein
+                    | POS = ADJA | Wortform = *bayer* | POS = N* | Lemma =
+                    *stadt
+                  </span>
+                </v-tooltip>
               </v-tab>
               <v-tab>
-                N=2
+                <v-tooltip right>
+                  <template v-slot:activator="{ on, attrs }">
+                    <span v-bind="attrs" v-on="on">N=2</span>
+                  </template>
+                  <span>
+                    <strong>Hinweis: </strong>N=2 erlaubt es, nach zwei direkt
+                    aufeinanderfolgende Token (Layer: Wortform, Lemma oder POS)
+                    zu suchen.<br />
+                    <strong>Beispiele: </strong>1. Wortform = angela &amp; 2.
+                    Wortform = merkel | 1. POS = ADJA &amp; 2. Lemma = lockdown
+                    | 1. Lemma = *präsident &amp; 2. POS = N*
+                  </span>
+                </v-tooltip>
               </v-tab>
               <v-tab>
-                N=3
+                <v-tooltip right>
+                  <template v-slot:activator="{ on, attrs }">
+                    <span v-bind="attrs" v-on="on">N=3</span>
+                  </template>
+                  <span>
+                    <strong>Hinweis: </strong>N=3 erlaubt es, nach drei direkt
+                    aufeinanderfolgende Token (Layer: Wortform, Lemma oder POS)
+                    zu suchen. Dabei kann die mittlere Position auch leer
+                    gelassen werden.<br />
+                    <strong>Beispiele: </strong>1. Wortform = sowohl &amp; 2.
+                    LEER &amp; 3. Wortform = auch | 1. POS = ADJA &amp; 2. Lemma
+                    = angela &amp; Lemma = merkel*
+                  </span>
+                </v-tooltip>
               </v-tab>
 
               <v-tab-item>
                 <v-card flat>
-                  <v-row style="margin-top:-30px">
+                  <v-row>
                     <v-col cols="2">
                       <v-overflow-btn
                         persistent-hint
                         :items="layer"
-                        :selection="{ index: 0 }"
+                        v-model="layerDefault"
                         label="Layer"
                         item-value="text"
                       ></v-overflow-btn>
@@ -34,51 +70,33 @@
                       <v-text-field label="Token"></v-text-field>
                     </v-col>
                   </v-row>
-                  <v-row>
-                    <v-col cols="2"></v-col>
-                    <v-col cols="10" style="margin-top:-45px">
-                      <v-alert icon="info" elevation="2" dismissible>
-                        N=1 erlaubt es, nach einzelnen Token (Layer: Wortform,
-                        Lemma oder POS) zu suchen.
-                      </v-alert>
-                    </v-col>
-                  </v-row>
-                  <v-row>
-                    <v-col cols="2"></v-col>
-                    <v-col cols="10" style="margin-top:-30px">
-                      <v-alert icon="auto_awesome" elevation="2" dismissible>
-                        <strong>Vorschläge:</strong> Lemma = Virus | POS = ADJA
-                        | Wortform = Merkel* | Lemma = sein | Wortform = *bayer*
-                      </v-alert>
-                    </v-col>
-                  </v-row>
                 </v-card>
               </v-tab-item>
               <v-tab-item>
                 <v-card flat>
-                  <v-row style="margin-top:20px">
+                  <v-row>
                     <v-col cols="2">
                       <v-overflow-btn
                         persistent-hint
                         :items="layer"
-                        :selection="{ index: 0 }"
+                        v-model="layerDefault"
                         label="Layer"
                         item-value="text"
                       ></v-overflow-btn>
                     </v-col>
-                    <v-col cols="4" style="margin-top:7px">
+                    <v-col cols="4">
                       <v-text-field label="Token (1. Position)"></v-text-field>
                     </v-col>
                     <v-col cols="2">
                       <v-overflow-btn
                         persistent-hint
                         :items="layer"
-                        :selection="{ index: 0 }"
+                        v-model="layerDefault"
                         label="Layer"
                         item-value="text"
                       ></v-overflow-btn>
                     </v-col>
-                    <v-col cols="4" style="margin-top:7px">
+                    <v-col cols="4">
                       <v-text-field label="Token (2. Position)"></v-text-field>
                     </v-col>
                   </v-row>
@@ -86,41 +104,41 @@
               </v-tab-item>
               <v-tab-item>
                 <v-card flat>
-                  <v-row style="margin-top:67px" cols="27">
+                  <v-row cols="27">
                     <v-col cols="2">
                       <v-overflow-btn
                         persistent-hint
                         :items="layer"
-                        :selection="{ index: 0 }"
+                        v-model="layerDefault"
                         label="Layer"
                         item-value="text"
                       ></v-overflow-btn>
                     </v-col>
-                    <v-col cols="2" style="margin-top:7px">
+                    <v-col cols="2">
                       <v-text-field label="Token (1. Position)"></v-text-field>
                     </v-col>
                     <v-col cols="2">
                       <v-overflow-btn
                         persistent-hint
                         :items="layer"
-                        :selection="{ index: 0 }"
+                        v-model="layerDefault"
                         label="Layer"
                         item-value="text"
                       ></v-overflow-btn>
                     </v-col>
-                    <v-col cols="2" style="margin-top:7px">
+                    <v-col cols="2">
                       <v-text-field label="Token (2. Position)"></v-text-field>
                     </v-col>
                     <v-col cols="2">
                       <v-overflow-btn
                         persistent-hint
                         :items="layer"
-                        :selection="{ index: 0 }"
+                        v-model="layerDefault"
                         label="Layer"
                         item-value="text"
                       ></v-overflow-btn>
                     </v-col>
-                    <v-col cols="2" style="margin-top:7px">
+                    <v-col cols="2">
                       <v-text-field label="Token (3. Position)"></v-text-field>
                     </v-col>
                   </v-row>
@@ -128,11 +146,10 @@
               </v-tab-item>
             </v-tabs>
           </v-col>
-          <v-col cols="1">
-            <v-btn block>
-              <v-icon>search</v-icon><br /><br /><br /><br /><br />
-              Suchen
-            </v-btn>
+        </v-row>
+        <v-row>
+          <v-col>
+            <v-btn block> <v-icon>search</v-icon>Suchen </v-btn>
           </v-col>
         </v-row>
       </v-expansion-panel-content>
@@ -174,6 +191,7 @@ export default {
   data: () => {
     return {
       layer: [{ text: "Wortform" }, { text: "Lemma" }, { text: "POS" }],
+      layerDefault: { text: "Wortform" },
       name: "world",
     };
   },
